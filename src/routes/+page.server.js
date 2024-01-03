3ade637c14e57acb9e327f4f96928e0c3e4b28c7
Brian Morearty <brian@morearty.org>
2024-01-03 08:48:26 -0800

    Reproduce bug

---
+++ src/routes/+page.server.js
@@ -0,0 +1,9 @@
+export const load = async ({}) => {
+  return {
+    items: [
+      { id: 1, name: 'foo', price: 100 },
+      { id: 2, name: 'bar', price: 200 },
+      { id: 3, name: 'baz', price: 300 },
+    ],
+  };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { cloneDeep } from 'lodash-es';
+  import { invalidateAll } from '$app/navigation';
+
+  let price = $state(200);
+  const { data } = $props();
+  let thing = $state({ items: cloneDeep(data.items) });
+  $effect(() => {
+    thing = { items: cloneDeep(data.items) };
+  });
+
+  function changePrice() {
+    thing.items[1].price = price;
+  }
+</script>
+
+<h2>Unkeyed <code>#each</code> (reactive)</h2>
+{#each thing.items as item}
+  <p>{item.name} costs ${item.price}</p>
+{/each}
+
+<h2>Keyed <code>#each</code> (not reactive)</h2>
+{#each thing.items as item (item.id)}
+  <p>{item.name} costs ${item.price}</p>
+{/each}
+
+<h2>Change item 2's price. The keyed <code>#each</code> won’t update:</h2>
+<form on:submit={changePrice}>
+  <input bind:value={price} />
+  <button type="submit">Change price</button>
+</form>
+
+<h2>Reload</h2>
+<button on:click={() => invalidateAll()}><code>invalidateAll()</code></button>
